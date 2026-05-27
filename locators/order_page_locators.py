d4b12070db6269a33cc79c284a67002819fc1dbb
Vera Minakova <vera31029@yandex.ru>
2026-05-27 22:33:13 +0300

    Minakova: Second commit in Sprint 6 with corrections: requirements.txt was filled; locators in main_page_locators.py and order_page_locators.py were converted to cortege

--- locators/order_page_locators.py
+++ locators/order_page_locators.py
@@ -1,11 +1,11 @@
 from selenium.webdriver.common.by import By
 
-INPUT_NAME = [By.XPATH, ".//input[@placeholder='* Имя']"]
-INPUT_SURNAME = [By.XPATH, ".//input[@placeholder='* Фамилия']"]
-INPUT_ADDRESS = [By.XPATH, ".//input[@placeholder='* Адрес: куда привезти заказ']"]
-INPUT_PHONE = [By.XPATH, ".//input[@placeholder='* Телефон: на него позвонит курьер']"]
+INPUT_NAME = (By.XPATH, ".//input[@placeholder='* Имя']")
+INPUT_SURNAME = (By.XPATH, ".//input[@placeholder='* Фамилия']")
+INPUT_ADDRESS = (By.XPATH, ".//input[@placeholder='* Адрес: куда привезти заказ']")
+INPUT_PHONE = (By.XPATH, ".//input[@placeholder='* Телефон: на него позвонит курьер']")
 ORDER_BUTTON = (By.XPATH, "//div[contains(@class,'Order_Buttons')]//button[normalize-space()='Заказать']")
-INPUT_METRO_STATION = [By.XPATH, ".//input[@class='select-search__input']"]
+INPUT_METRO_STATION = (By.XPATH, ".//input[@class='select-search__input']")
 LOGO_SCOOTER = (By.XPATH, "//a[contains(@class,'Header_LogoScooter')]")
 YANDEX_LOGO = (By.CSS_SELECTOR, "a.Header_LogoYandex__3TSOI")
 BLACK_COLOUR_CHECKBOX = (By.XPATH, "//label[@for='black']")
